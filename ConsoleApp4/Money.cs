cac4cd9e08292db8b16afd40a7750fd04f853ad9
Тынышбаев Алибек Алмасулы <@corp.itstep>
2019-02-23 15:01:46 +0600

    практика модуль 4

---
+++ ConsoleApp4/Money.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Money
+    {
+        private int _countMoney;
+        private int _parMoney;
+        public Money()
+        {
+            _countMoney = 0;
+            _parMoney = 0;
+        }
+        public Money(int countMoney, int parMoney)
+        {
+            _countMoney = countMoney;
+            _parMoney = parMoney;
+        }
+        public void Output()
+        {
+            Console.WriteLine(_countMoney);
+            Console.WriteLine(_parMoney);
+        }
+        public void CostProduct(int price)
+        {
+            if (_parMoney * _countMoney > price)
+            {
+                Console.WriteLine("Вам хватит денежных средств на покупку товара стоимостью " + price + "тг.");
+            }
+            else if (_parMoney * _countMoney < price)
+            {
+                Console.WriteLine("Вам не хватит денежных средств на покупку товара стоимостью " + price + "тг.");
+            }
+
+        }
+        public void CountProduct(int price)
+        {
+            int count = (_parMoney * _countMoney) / price;
+            Console.WriteLine(count + " штук товара стоимостью " + price + "тг можно купить на имеющиеся денежные средства");
+        }
+
+
+
+        public int GetCountMoney()
+        {
+            return _countMoney;
+        }
+        public int GetParMoney()
+        {
+            return _parMoney;
+        }
+        public void SetCountMoney(int countMoney)
+        {
+            _countMoney = countMoney;
+        }
+        public void SetParMoney(int parMoney)
+        {
+            _parMoney = parMoney;
+        }
+        public int valueMoney()
+        {
+            return _parMoney * _countMoney;
+        }
+
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new Money();
+
+            money.SetCountMoney(7);
+            money.SetParMoney(500);
+            Console.WriteLine("Количество купюр: " + money.GetCountMoney());
+            Console.WriteLine("Номинал купюр: " + money.GetParMoney() + "тг.");
+            Console.WriteLine("Денег у вас: " + money.valueMoney() + "тг.");
+            money.CostProduct(5000);
+            money.CountProduct(500);
+
+            Console.ReadKey();
+        }
+    }
+}
